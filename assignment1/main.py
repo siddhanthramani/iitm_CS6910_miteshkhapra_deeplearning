f250ad05aea62e5e96a2756aa96ee4557b5c65ee
Siddhanth Ramani <siddhanth@mindgrovetech.in>
2023-02-23 10:23:11 +0530

    Feb 3rd - made back prop complete

--- assignment1/main.py
+++ assignment1/main.py
@@ -7,10 +7,12 @@ num_neurons_dict = {0 : 28*28, 1 : 40, 2 : 10}
 activation_dict = {0 : "linear", 1 : "logistic", 2 : "softmax"}
 print(data["train_X"].shape)
 print(data["train_y"].shape)
-nn1 = neural_network(num_neurons_dict, activation_dict)
-output = nn1.predict(data["train_X"][0])
+print(data["train_X"][0:10].shape)
+
+nn1 = neural_network(num_neurons_dict, activation_dict, nn_init_random_max=100)
+# output = nn1.predict(data["train_X"][0])
 # print(output)
 
-nn1.fit(data["train_X"], data["train_y"], eta=0.1)
+nn1.fit(data["train_X"][0:10], data["train_y"][0:10], eta=0.1, epochs=20)
 output = nn1.predict(data["train_X"][0])
 print(output)
--- assignment1/nn.py
+++ assignment1/nn.py
@@ -16,6 +16,7 @@ class neural_network():
         self.__gradient_descent_type_dict = {
             "sgd" : self.__sgd_update
         }
+        self.grad_loss_update_first_time = 0
         self.param_dict = dict()
 
         for layer, num_neurons in sorted(num_neurons_dict.items()):
@@ -23,7 +24,8 @@ class neural_network():
                 continue
             self.W[layer] = nn_init(num_neurons_dict[layer-1], num_neurons) * nn_init_random_max
             self.b[layer] = nn_init(num_neurons, 1) * nn_init_random_max
-    
+
+        print("hi")
             
     def __get_activation_function(self, layer):
         if self.activation_type == "constant":
@@ -47,20 +49,7 @@ class neural_network():
             return (np.exp(-input)/ np.exp(-input).sum())
 
 
-    def predict(self, x_i, print_transforms=1):
-        for layer in self.layers:
-            ip_shape = x_i.shape
-            x_i = np.add(np.dot(np.transpose(self.W[layer]), x_i), self.b[layer])
-            activation = self.__get_activation_function(layer)
-            x_i = self.__activation_function(x_i, activation)
-            op_shape = x_i.shape
-            if print_transforms:
-                print("Layer : {} - Input_Shape : {}, W shape : {}, b shape : {}, and output_shape : {}"
-                      .format(layer, ip_shape, self.W[layer].shape, self.b[layer].shape, op_shape))
-        return x_i
-
-
-    def __forward_pass(self, x_i, print_transforms=1):
+    def __forward_pass(self, x_i):
         a_i = dict()
         h_i = dict()
         h_i[0] = x_i
@@ -69,9 +58,6 @@ class neural_network():
             activation = self.__get_activation_function(layer)
             h_i[layer] = self.__activation_function(a_i[layer], activation)
 
-            if print_transforms:
-                print("Layer : {} - Input_Shape : {}, W shape : {}, b shape : {}, and output_shape : {}"
-                      .format(layer, a_i[layer].shape, self.W[layer].shape, self.b[layer].shape, h_i[layer].shape))
         return a_i, h_i
     
 
@@ -83,13 +69,19 @@ class neural_network():
             inter = self.__activation_function(ak, activation)
             return (1 - np.square(inter))
             
-
-    def __common_backward_pass(self, a_i, h_i, y):
+    def __get_loss(self, a_i, h_i, y):
         if self.loss_function == "cross_entropy":
             if not self.activation_dict[self.max_layer] == "softmax":
                 raise Exception("Cross entropy can be used only if the final activation function is softmax")
 
             loss = -np.sum(np.multiply(y, np.log(h_i[self.max_layer])))
+        return loss
+    
+    def __common_backward_pass(self, a_i, h_i, y):
+        if self.loss_function == "cross_entropy":
+            if not self.activation_dict[self.max_layer] == "softmax":
+                raise Exception("Cross entropy can be used only if the final activation function is softmax")
+
             grad_loss_h = dict()
             grad_loss_W = dict()
             grad_loss_b = dict()
@@ -99,9 +91,9 @@ class neural_network():
 
             for layer in self.layers:
                 grad_layer = self.max_layer - layer + 1
-                grad_loss_W = np.dot(grad_loss_a[grad_layer], np.transpose(h_i[grad_layer-1]))
-                grad_loss_b = grad_loss_a[grad_layer]
-                if layer == 1:
+                grad_loss_W[grad_layer] = np.dot(h_i[grad_layer-1], np.transpose(grad_loss_a[grad_layer]))
+                grad_loss_b[grad_layer] = grad_loss_a[grad_layer]
+                if grad_layer == 1:
                     break
                 grad_loss_h[grad_layer-1] = np.dot(self.W[grad_layer], grad_loss_a[grad_layer])
                 grad_loss_a[grad_layer-1] = np.multiply(grad_loss_h[grad_layer-1], 
@@ -115,45 +107,67 @@ class neural_network():
             grad_loss_b = dict()
             print("THIS HAS NOT BEEN CODED YET")
 
-        return loss, grad_loss_b, grad_loss_W 
+        return grad_loss_W, grad_loss_b 
 
 
-    def __gradient_update(self, global_grad_loss_W, global_grad_loss_b, grad_loss_W, grad_loss_b):
+    def __gradient_loss_update(self, global_grad_loss_W, global_grad_loss_b, grad_loss_W, grad_loss_b):
         for layer in self.layers:
-            global_grad_loss_W[layer] += grad_loss_W[layer]
-            global_grad_loss_b[layer] += grad_loss_b[layer]
+            if self.grad_loss_update_first_time:
+                global_grad_loss_W[layer] += grad_loss_W[layer]
+                global_grad_loss_b[layer] += grad_loss_b[layer]
+            else:
+                global_grad_loss_W[layer] = grad_loss_W[layer]
+                global_grad_loss_b[layer] = grad_loss_b[layer]
+        self.grad_loss_update_first_time = 1
+
         return global_grad_loss_W, global_grad_loss_b
         
 
     def __sgd_update(self, global_grad_loss_W, global_grad_loss_b):
         for layer in self.layers:
-            self.W[layer] -= np.multiply(self.param_dict["eta"], global_grad_loss_W[layer])
-            self.b[layer] -= np.multiply(self.param_dict["eta"], global_grad_loss_b[layer])
+            W_eta_shape = self.W[layer].shape
+            b_eta_shape = self.b[layer].shape
+            self.W[layer] -= np.multiply(np.full(W_eta_shape, self.param_dict["eta"]), global_grad_loss_W[layer])
+            self.b[layer] -= np.multiply(np.full(b_eta_shape, self.param_dict["eta"]), global_grad_loss_b[layer])
 
         
     def fit(self, all_x, all_y, minibatch_size=0, epochs=1, gradient_descent_type="sgd", **kwargs):
+        self.grad_loss_update_first_time = 0
         for param, param_val in kwargs.items():
             self.param_dict[param] = param_val
         
         if not minibatch_size:
             minibatch_size = len(all_y)
 
-        for i in range(epochs):
-            global_grad_loss_b = dict()
-            global_grad_loss_W = dict()
-
+        global_grad_loss_b = dict()
+        global_grad_loss_W = dict()
+        num_points_seen = 0
+        for i in range(epochs):            
             for x_i, y in zip(all_x, all_y):
                 y = np.array([1 if i==(y-1) else 0 for i in range(10)]).reshape(10, 1)
-                a_i, h_i = self.__forward_pass(x_i, print_transforms=1)
-                loss, grad_loss_b, grad_loss_W = self.__common_backward_pass(a_i, h_i, y)
-                global_grad_loss_W, global_grad_loss_b =  self.__gradient_update(global_grad_loss_W, global_grad_loss_b, 
+                a_i, h_i = self.__forward_pass(x_i)
+                grad_loss_W, grad_loss_b = self.__common_backward_pass(a_i, h_i, y)
+                global_grad_loss_W, global_grad_loss_b =  self.__gradient_loss_update(global_grad_loss_W, global_grad_loss_b, 
                                                                             grad_loss_W, grad_loss_b)
                 num_points_seen += 1
 
                 if num_points_seen % minibatch_size == 0:            
                     self.__gradient_descent_type_dict[gradient_descent_type](global_grad_loss_W, global_grad_loss_b)
+                    num_points_seen = 0
+                    print("Loss is : ", self.__get_loss(a_i, h_i, y))
 
         print("Model fitting is over.")
 
-        
+
+    def predict(self, x_i, print_transforms=1):
+        for layer in self.layers:
+            ip_shape = x_i.shape
+            x_i = np.add(np.dot(np.transpose(self.W[layer]), x_i), self.b[layer])
+            activation = self.__get_activation_function(layer)
+            x_i = self.__activation_function(x_i, activation)
+            op_shape = x_i.shape
+            if print_transforms:
+                print("Layer : {} - Input_Shape : {}, W shape : {}, b shape : {}, and output_shape : {}"
+                      .format(layer, ip_shape, self.W[layer].shape, self.b[layer].shape, op_shape))
+        return x_i
         
