b3056d18708f1258b24df266b27bff1b7c63f1b9
Siddhanth Ramani <siddhanth@mindgrovetech.in>
2023-03-11 14:51:43 +0530

    Cleaned rmsprop and edited history updates

--- assignment1/nn_core/nn_optimizer.py
+++ assignment1/nn_core/nn_optimizer.py
@@ -179,20 +179,26 @@ class RMSProp:
         self.beta = beta
         self.eta = eta
         self.nn_instance = neural_network_instance
-        self.step_reset()
+        self.setup()
+
+
+    def setup(self):
+        self.num_points_seen = 0
+        self.total_grad_loss_W = dict()
+        self.total_grad_loss_b = dict()
+        self.prev_total_grad_loss_W = dict()
+        self.prev_total_grad_loss_b = dict()
+        for layer in self.nn_instance.layers:
+            self.total_grad_loss_W[layer] = np.zeros(self.nn_instance.W[layer].shape)
+            self.total_grad_loss_b[layer] = np.zeros(self.nn_instance.b[layer].shape)
+            self.prev_total_grad_loss_W[layer] = np.zeros(self.nn_instance.W[layer].shape)
+            self.prev_total_grad_loss_b[layer] = np.zeros(self.nn_instance.b[layer].shape)
 
 
     def grad_update(self, grad_loss_W, grad_loss_b):
         for layer in self.nn_instance.layers:
-            # For first time update, we init accumalated value
-            if self.grad_update_first_time:
-                self.total_grad_loss_W[layer] = grad_loss_W[layer]
-                self.total_grad_loss_b[layer] = grad_loss_b[layer]
-            # For all other times, we accumulate the gradient
-            else:
-                self.total_grad_loss_W[layer] += grad_loss_W[layer]
-                self.total_grad_loss_b[layer] += grad_loss_b[layer]
-        self.grad_update_first_time = 0
+            self.total_grad_loss_W[layer] += grad_loss_W[layer]
+            self.total_grad_loss_b[layer] += grad_loss_b[layer]
         self.num_points_seen += 1        
 
 
@@ -216,12 +222,9 @@ class RMSProp:
         self.num_points_seen = 0
         self.total_grad_loss_W = dict()
         self.total_grad_loss_b = dict()
-        self.prev_total_grad_loss_W = dict()
-        self.prev_total_grad_loss_b = dict()
         for layer in self.nn_instance.layers:
-                self.prev_total_grad_loss_W[layer] = np.zeros(self.nn_instance.W[layer].shape)
-                self.prev_total_grad_loss_b[layer] = np.zeros(self.nn_instance.b[layer].shape)
-        self.grad_update_first_time = 1
+            self.total_grad_loss_W[layer] = np.zeros(self.nn_instance.W[layer].shape)
+            self.total_grad_loss_b[layer] = np.zeros(self.nn_instance.b[layer].shape)
 
 
 
